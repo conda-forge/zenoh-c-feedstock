8bdbed04edbfb4ff80725ca426cd14e6977fb481
Daisuke Nishimatsu <daisuke@DaisukenoMacBook-Air.local>
2026-04-27 00:26:39 +0900

    update
    Signed-off-by: Daisuke Nishimatsu <daisuke@DaisukenoMacBook-Air.local>

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -7,9 +7,3 @@ cd examples
 # Compile example that links zeno
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release .
 cmake --build . --config Release
-
-# Run example that exit immediately
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-    ./z_info
-    ./z_bytes
-fi
